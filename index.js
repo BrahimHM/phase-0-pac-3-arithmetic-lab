63a8357b75347bebf0d9b23b89b1f1eb8fdce525
Brahim HM <brahimhm98@gmail.com>
2023-08-05 22:53:10 +0100

    completed

---
+++ index.js
@@ -0,0 +1,8 @@
+const add=(a,b)=>a+b
+const subtract=(a,b)=>a-b
+const multiply=(a,b)=>a*b
+const divide=(a,b)=>a/b
+const increment=a=>++a
+const decrement=a=>--a
+const makeInt=a=>parseInt(a,10)
+const preserveDecimal=a=>parseFloat(a)
